2f548edc12b8956aaa993fe69cc10c067194a29d
Patrick Whitesell <patrick.whitesell@meetearnest.com>
2018-10-31 13:52:57 -0700

    Fix PredicateOps

--- src/main/scala/com/earnest/microtypical/syntax/earnest/predicate.scala
+++ src/main/scala/com/earnest/microtypical/syntax/earnest/predicate.scala
@@ -1,14 +1,16 @@
 package com.earnest.microtypical.syntax.earnest
 
 import com.earnest.microtypical.Predicate
+
 import scala.language.implicitConversions
 
 final class PredicateOps [A] (val self : Predicate [A]) extends AnyVal {
   def not: Predicate [A] = a => ! self (a)
+  def `unary_!`: Predicate [A] = a => ! self (a)
 }
 
 trait ToPredicateOps {
-  implicit def PredicateOps [A](self: Predicate [A]): PredicateOps [A] =
+  implicit def toPredicateOps [A, B <: Boolean] (self: A => B): PredicateOps [A] =
     new PredicateOps [A] (self)
 }
 
